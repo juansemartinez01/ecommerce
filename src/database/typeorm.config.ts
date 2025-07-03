11578ec2f48790ecddcee0c3fbb8d1d497c787ff
Juan Sebastian Martínez <juansemar123456789@gmail.com>
2025-07-03 09:59:47 -0300

    Fix cadena de conexion

--- src/database/typeorm.config.ts
+++ src/database/typeorm.config.ts
@@ -1,12 +1,21 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { parse } from 'pg-connection-string';
+
+const dbUrl = process.env.DATABASE_URL || '';
+
+const config = parse(dbUrl);
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: '1234',
-  database: 'ecommerce',
+  host: config.host ?? undefined,
+  port: parseInt(config.port || '5432'),
+  username: config.user,
+  password: config.password,
+  database: config.database ?? undefined,
+
   autoLoadEntities: true,
-  synchronize: true, // ⚠️ en producción cambiar a false
+  synchronize: true, // ⚠️ en producción siempre false
+  ssl: {
+    rejectUnauthorized: false, // necesario para Railway
+  },
 };
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ['http://localhost:3001','http://localhost:3000','http://frontend-ecommerce-mutuo-production.up.railway.app'], // o true para permitir todos los orígenes (no recomendado en producción)
+    credentials: true, // si usas cookies o autenticación con tokens en headers
+  });
+  const reflector = app.get(Reflector);
+  // <— Aquí aplicamos el guard de JWT a todo, pero respetando @Public()
+
+  
+  app.useGlobalGuards(new JwtAuthGuard(reflector));
+  //app.use(cookieParser());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
