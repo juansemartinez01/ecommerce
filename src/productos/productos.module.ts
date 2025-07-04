a4eb2f716818f09ff0dbffd1c7734284dc36cec7
Juan Sebastian Martínez <juansemar123456789@gmail.com>
2025-07-04 14:51:08 -0300

    Modificaciones prodcuto, imagen, color

--- src/productos/productos.module.ts
+++ src/productos/productos.module.ts
@@ -3,12 +3,23 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Producto } from './entidades/producto.entity';
 import { Categoria } from './entidades/categoria.entity';
 import { Talle } from './entidades/talle.entity';
-import { ProductoTalle } from './entidades/producto-talle.entity';
+import { Color } from './entidades/color.entity';
+import { ProductoColorTalle } from './entidades/producto-color-talle.entity';
+import { ImagenProducto } from './entidades/imagen-producto.entity';
 import { ProductosService } from './productos.service';
 import { ProductosController } from './producto.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Producto, Categoria, Talle, ProductoTalle])],
+  imports: [
+    TypeOrmModule.forFeature([
+      Producto,
+      Categoria,
+      Talle,
+      Color,
+      ProductoColorTalle,
+      ImagenProducto,
+    ]),
+  ],
   providers: [ProductosService],
   controllers: [ProductosController],
 })
