87f67aae61cb8222eb791b68360a512ee7d65b30
Juan Sebastian Martínez <juansemar123456789@gmail.com>
2025-07-10 17:02:56 -0300

    Fixes productos y files

--- src/pedidos/pedidos.controller.ts
+++ src/pedidos/pedidos.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
 import { PedidosService } from './pedidos.service';
 import { ConfirmarPedidoDto } from './dto/confirmar-pedido.dto';
+import { UpdateEstadoPedidoDto } from './dto/update-estado-pedido.dto';
 
 @Controller('pedidos')
 export class PedidosController {
@@ -16,8 +17,16 @@ export class PedidosController {
     return this.service.listarTodos();
   }
 
+  @Patch(':id/estado')
+  actualizarEstado(
+    @Param('id') id: string,
+    @Body() dto: UpdateEstadoPedidoDto,
+  ) {
+    return this.service.actualizarEstado(+id, dto.estado);
+  }
+
   @Post('cancelar/:id')
-async cancelar(@Param('id') id: number) {
-  return this.service.cancelarPedido(+id);
-}
+  async cancelar(@Param('id') id: number) {
+    return this.service.cancelarPedido(+id);
+  }
 }
