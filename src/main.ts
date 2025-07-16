595e80ed7eea4de42bc08fed14099347ea14b360
Joaquin Dalmasso <129230938+JoaquinDalmasso@users.noreply.github.com>
2025-07-16 16:57:29 -0300

    Agrega la s en el http del front

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { JwtAuthGuard } from './auth/jwt-auth.guard';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3001','http://localhost:3000','http://frontend-ecommerce-mutuo-production.up.railway.app'], // o true para permitir todos los orígenes (no recomendado en producción)
+    origin: ['http://localhost:3001','http://localhost:3000','https://frontend-ecommerce-mutuo-production.up.railway.app'], // o true para permitir todos los orígenes (no recomendado en producción)
     credentials: true, // si usas cookies o autenticación con tokens en headers
   });
   const reflector = app.get(Reflector);
